91466a831b5a5d9b83b6d8b0e131267504e5cc79
nemanja94 <aandric.nemanja@gmail.com>
2021-01-01 14:17:03 +0100

    Dodavanje

---
+++ functions.js
@@ -0,0 +1,9 @@
+// Mobile Menu Toggle Button JavaScript
+function myFunction() {
+  var x = document.getElementById("mobile-menu");
+  if (x.style.visibility === "hidden") {
+    x.style.visibility = "visible";
+  } else {
+    x.style.visibility = "hidden";
+  }
+}

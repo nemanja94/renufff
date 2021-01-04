eb5ee9146bd4e2c9022899200798d2682f1d25d2
nemanja94 <aandric.nemanja@gmail.com>
2021-01-04 21:34:51 +0100

    dodat carousel kao web komponenta

---
+++ app-carousel/app-acarousel.js
@@ -0,0 +1,126 @@
+const template = document.createElement('template');
+template.innerHTML = `
+<style>
+    section {
+        padding: 0;
+        margin: 20px 0 0 -45%;
+        position: absolute;
+        width: 90%;
+        height: 50vh;
+        display: flex;
+        border: 5px solid #bc9d66;
+        border-radius: 5px;
+    }
+
+    section .slide {
+        position: relative;
+        height: 100%;
+        flex: 1;
+        border-right: 2px solid #bc9d66;
+        overflow: hidden;
+        transition: 0.5s ease-in-out;
+    }
+
+    section .slide:last-child {
+        border-right: none;
+    }
+
+    section .slide:hover {
+        flex-grow: 5;
+    }
+
+    section .slide:nth-child(1) {
+        background-image: url('../images/1.jpg');
+        background-position: center center;
+    }
+
+    section .slide:nth-child(2) {
+        background-image: url('../images/2.jpg');
+        background-position: center center;
+    }
+
+    section .slide:nth-child(3) {
+        background-image: url('../images/3.jpg');
+        background-position: center center;
+    }
+
+    section .slide .content {
+        position: absolute;
+        bottom: 0;
+        margin: 40px;
+        padding: 30px;
+        background: #bc9d66;
+        border: 3px solid #161616;
+        border-radius: 5px;
+        color: white;
+        visibility: hidden;
+        opacity: 0;
+        transition: 0s;
+        transform: translateY(100px);
+    }
+
+    section .slide:hover .content {
+        visibility: visible;
+        opacity: 1;
+        transition: 0.3s;
+        transition-delay: 0.3s;
+        transform: translateY(0);
+    }
+
+    @media screen and (max-width: 768px) {
+        section {
+            margin: -60px 0 0 -45%;
+            width: 87.5%;
+            flex-direction: column;
+            height: 70vh;
+            border: 4px solid #bc9d66;
+            border-radius: 3px;
+        }
+
+        section .slide {
+            border-right: none;
+            border-bottom: 2px solid #bc9d66;
+        }
+
+        section .slide:last-child {
+            border-bottom: none;
+        }
+    }
+
+</style>
+
+<section>
+    <div class="slide">
+        <div class="content">
+            <h2>Slide One</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque non dignissimos delectus quam odio
+            culpa, facilis tenetur voluptatem.</p>
+        </div>
+    </div>
+    <div class="slide">
+        <div class="content">
+            <h2>Slide One</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque non dignissimos delectus quam odio
+            culpa, facilis tenetur voluptatem.</p>
+        </div>
+    </div>
+    <div class="slide">
+        <div class="content">
+            <h2>Slide One</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque non dignissimos delectus quam odio
+            culpa, facilis tenetur voluptatem.</p>
+        </div>
+    </div>
+</sextion>
+`;
+
+class Carousel extends HTMLElement {
+    constructor() {
+        super();
+
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+}
+
+window.customElements.define('app-carousel', Carousel);
--- app.js
+++ app.js
@@ -2,41 +2,10 @@ const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
 
+// Hamburger menu conf
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('open');
     links.forEach(link => {
         link.classList.toggle('fade');
     });
 });
-
-const asd = document.querySelector('.nav-links li:nth-child(1)');
-console.log(asd);
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
-}

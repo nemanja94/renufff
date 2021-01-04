82ae96fe00bf6a64dcfc947c023aa1a9e8c50cd3
nemanja94 <aandric.nemanja@gmail.com>
2021-01-04 14:14:11 +0100

    dodatk carousel

--- app.js
+++ app.js
@@ -6,28 +6,37 @@ hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('open');
     links.forEach(link => {
         link.classList.toggle('fade');
-    })
-})
+    });
+});
 
-var slideIndex = 0;
-showSlides();
+const asd = document.querySelector('.nav-links li:nth-child(1)');
+console.log(asd);
+
+var slideIndex = 1;
+showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideIndex - 1].alt;
 }
